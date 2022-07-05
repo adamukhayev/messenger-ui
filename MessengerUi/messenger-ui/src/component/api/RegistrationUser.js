85ef72b4091acd5df0455311ba4e5d7b375d7650
adamukhayev <67798828+adamukhayev@users.noreply.github.com>
2022-07-06 00:08:51 +0600

    Delete MessengerUi/messenger-ui/src directory

--- MessengerUi/messenger-ui/src/component/api/RegistrationUser.js
+++
@@ -1,7 +0,0 @@
-import BaseApi from "./BaseApi";
-
-export default class RegistrationUser extends BaseApi {
-  static registrationUser(data) {
-    return this.post('/api/v1/auth/register', data)
-  }
-}
